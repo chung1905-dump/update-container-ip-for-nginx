bb65b0a0a9ccd3d362fcfead2023e35f6ac4c1c3
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2021-03-01 14:59:18 +0700

    feat: create ContainerIp tuple

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from typing import Iterator
+from typing import Iterator, NamedTuple, List
 
 import docker
 from docker import DockerClient
 from docker.models.containers import Container
 
-if __name__ == '__main__':
-    client: DockerClient = docker.from_env()
-    containers: Iterator[Container] = client.containers.list(filters={'status': 'running'})
-    containers = filter(lambda c: '80/tcp' in c.ports, containers)
-    for container in containers:
+
+class ContainerIp(NamedTuple):
+    name: str
+    ip: str
+
+
+def _map_container() -> List[ContainerIp]:
+    ret: List[ContainerIp] = []
+    ip: str
+    for container in docker_containers:
         networks: dict = container.attrs.get('NetworkSettings', {}).get('Networks', {})
         for network_name in networks:
-            print(network_name)
-            print(networks.get(network_name, {}).get('IPAddress', {}))
+            ip = networks.get(network_name, {}).get('IPAddress', None)
+            if ip is None:
+                continue
+            ret.append(ContainerIp(container.name, ip))
+    return ret
+
+
+if __name__ == '__main__':
+    client: DockerClient = docker.from_env()
+    docker_containers: Iterator[Container] = client.containers.list(filters={'status': 'running'})
+    docker_containers = filter(lambda c: '80/tcp' in c.ports, docker_containers)
+    containers = _map_container()
+    print(containers)
